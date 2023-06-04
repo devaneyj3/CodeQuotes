4f94bf3360b044b6ac33eb04863451052576cac6
Jordan Devaney <jordandevaney28@gmail.com>
2023-06-04 16:52:47 -0400

    work with gradients

---
+++ MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿namespace CodeQuotes;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void btnGenerateQuote_Clicked(System.Object sender, System.EventArgs e)
+    {
+
+		var random = new Random();
+		var startColor =
+			System.Drawing.Color.FromArgb(
+				random.Next(0, 256),
+				random.Next(0, 256),
+				random.Next(0, 256));
+
+        var endColor =
+            System.Drawing.Color.FromArgb(
+                random.Next(0, 256),
+                random.Next(0, 256),
+                random.Next(0, 256));
+
+		var colors = ColorUtility
+			.ColorControls
+			.GetColorGradient(startColor, endColor, 6);
+
+		float stopOffset = .0f;
+		var stops = new GradientStopCollection();
+
+		foreach (var c in colors)
+		{
+			stops.Add(new GradientStop(Color.FromArgb(c.Name),
+				stopOffset));
+			stopOffset += .2f;
+		}
+
+		var gradient =
+			new LinearGradientBrush(stops,
+				new Point(0, 0),
+				new Point(1, 1));
+
+		background.Background = gradient;
+
+    }
+}
+
+
